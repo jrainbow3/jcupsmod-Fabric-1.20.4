8771f26d9acef54c533a5d9ff1636e678c96a5c2
JacobRainbow <jacob@rtrt.me>
2023-12-08 23:32:33 -0500

    First commit

---
+++ src/main/java/net/jcup/testmod/JcupsModClient.java
@@ -0,0 +1,10 @@
+package net.jcup.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JcupsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/jcup/testmod/JcupsModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.jcup.testmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class JcupsModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
